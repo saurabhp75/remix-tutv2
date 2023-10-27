95485a479615c9e0309a403309c32e5ba0e3b04a
Saurabh Prakash <saurabhp75@gmail.com>
2023-10-27 20:24:22 +0530

    Add tailwindcss

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,15 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "RemixV2 tutorial" },
+    { name: "description", content: "Sample RemixV2 application" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div >
+      <h1 className="text-3xl text-zinc-500 font-extrabold">Hello from RemixV2</h1>
     </div>
   );
 }
